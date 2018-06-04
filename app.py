92eac8c45962bf4cbc0039f837f06d0efc2ebe59
Big <bigmorning@live.com>
2018-06-04 19:50:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -64,79 +64,17 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-
-    def inputmes(brin):
-        try:
-            conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-        except:
-            print("I am unable to connect to the database")
-        cur = conn.cursor()
-        
-        cur.execute("CREATE TABLE IF NOT EXISTS inputmes (word text, time TIMESTAMP NOT NULL);")
-
-        cur.execute("INSERT INTO inputmes (word, time) VALUES (%(str)s, NOW());", {'str':brin})
-        conn.commit()
-        
-        cur.close()
-        conn.close()
     
-    def usinputcur():
+    def inputtamtop(brin):
         try:
             conn = psycopg2.connect(DATABASE_URL, sslmode='require')
         except:
             print("I am unable to connect to the database")
         cur = conn.cursor()
         
-        #from https://stackoverflow.com/questions/6267887/get-last-record-of-a-table-in-postgres
-        cur.execute("SELECT word FROM inputmes ORDER BY time DESC LIMIT 1;")
-        m = cur.fetchall()
-        n = str(m)[3:-4]
-        conn.commit()
-        cur.close()
-        conn.close()
-        return n
-    
-    def inputoutmes(brin):
-        try:
-            conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-        except:
-            print("I am unable to connect to the database")
-        cur = conn.cursor()
-        
-        cur.execute("CREATE TABLE IF NOT EXISTS inputoutmes (word text, time TIMESTAMP NOT NULL);")
+        cur.execute("CREATE TABLE IF NOT EXISTS inputtamtop (tam text, time TIMESTAMP NOT NULL);")
 
-        cur.execute("INSERT INTO inputoutmes (word, time) VALUES (%(str)s, NOW());", {'str':brin})
-        conn.commit()
-        
-        cur.close()
-        conn.close()
-  
-    def usinputoutcur():
-        try:
-            conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-        except:
-            print("I am unable to connect to the database")
-        cur = conn.cursor()
-        
-        #from https://stackoverflow.com/questions/6267887/get-last-record-of-a-table-in-postgres
-        cur.execute("SELECT word FROM inputoutmes ORDER BY time DESC LIMIT 1;")
-        m = cur.fetchall()
-        n = str(m)[3:-4]
-        conn.commit()
-        cur.close()
-        conn.close()
-        return n
-    
-    def inputtamtop(brin, mo):
-        try:
-            conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-        except:
-            print("I am unable to connect to the database")
-        cur = conn.cursor()
-        
-        cur.execute("CREATE TABLE IF NOT EXISTS inputtamtop (tam text, top text, time TIMESTAMP NOT NULL);")
-
-        cur.execute("INSERT INTO inputtamtop (tam, top, time) VALUES (%(str)s, %(top)s, NOW());", {'str':brin, 'top':mo})
+        cur.execute("INSERT INTO inputtamtop (tam, time) VALUES (%(str)s, NOW());", {'str':brin})
         conn.commit()
         
         cur.close()
@@ -158,23 +96,8 @@ def handle_message(event):
             na = na.append(n)
             return na
         conn.commit()
-
-        #from https://stackoverflow.com/questions/6267887/get-last-record-of-a-table-in-postgres
-        cur.execute("SELECT top FROM inputtamtop ORDER BY time;")
-        h = cur.fetchall()
-        ba = []
-        for o in h:    
-            b = str(o)[3:-4]
-            ba = ba.append(b)
-            return ba
-        conn.commit()
-        
-        for y, u in na, ba:
-            chatbot.train([y, u])  
-        
-        #p = [n,b]
-        
-        #chatbot.train(p)
+    
+        chatbot.train(na)  
         
         cur.close()
         conn.close()
@@ -182,11 +105,7 @@ def handle_message(event):
     
     n = event.message.text
     
-    inputmes(n) #สิ่งที่เราตอบไป ต้องอยู่หลัง
-    mo = usinputoutcur() #มาจาก inputoutmes อยู่หน้า
-    lin = usinputcur() #มาจาก inputmes อยู่หลัง
-    cvst = [mo, lin]
-    inputtamtop(mo, lin)
+    inputtamtop("เบิกบาน")
     
     usinputtamtop()
     
@@ -194,7 +113,6 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=a))
-    inputoutmes(a) #คำถาม ต้องอยู่หน้า แต่เก็บค่าทีหลัง
 
     
 if __name__ == "__main__":
