475a30fe975f10b2005c77a987e9be2a213e8f0b
bigmore <bigmorning@live.com>
2020-05-10 19:46:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ def handle_message(event):
     noam = find_mess(n2, "me", 5, 1)
     h1, h2, h3, h4, h5, h6, h7 = noam[0]
     token_sente = word_tokenize(noam[0][3], engine='attacut')
-    o_list = ['ทดสอบ', str(token_sente)]
+    o_list = ['ทดสอบ', token_sente[0]]
 
     if e5 == h5:
         o_list_tsm = []
