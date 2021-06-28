87b5b80a844cf885fa7f4a7af9072493921b170e
bigmore <bigmorning@live.com>
2021-06-28 20:49:30 +0700

    o

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ app = Flask(__name__)
 def upload_file():
     return 'google-site-verification: googleff9deb20e4a46255.html'
 
+@app.route("/", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Goog-Resource-State']
+    body = request.get_data(as_text=True)
+    print(signature)
+    print(body)
+
+
 
 if __name__ == '__main__':
     app.run()
