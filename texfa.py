94933cac19b8131546f63d62e9b2132a1201039b
bigmore <bigmorning@live.com>
2021-06-28 14:10:18 +0700

    Update texfa.py

--- texfa.py
+++ texfa.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/googleff9deb20e4a46255.html', methods=['GET', 'POST'])
+@app.route('/googleff9deb20e4a46255.html')
 def upload_file():
-    f = ''
-    if request.method == 'POST':
-        f = request.files['googleff9deb20e4a46255.html']
-        f.save('googleff9deb20e4a46255.html')
-    return f
+    return 'google-site-verification: googleff9deb20e4a46255.html'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
